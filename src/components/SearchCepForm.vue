c78d38ce9c96f4c15700980b0c10606b7e060928
edianassouza <edianadasilvadesouza@gmail.com>
2020-05-01 16:23:54 +0000

    first commit

---
+++ src/components/SearchCepForm.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="field has-addons is-fullwidth">
+    <div class="control is-expanded">
+      <input
+        class="input"
+        v-model="inputCep"
+        @keypress.enter="foundNewInputCep"
+        type="text"
+        placeholder="Digite o CEP"
+      />
+    </div>
+    <div class="control">
+      <a class="button" @click="foundNewInputCep">Buscar</a>
+    </div>
+  </div>
+</template>
+
+<script>
+import { searchCep } from "@/services/ceps";
+
+export default {
+  data() {
+    return {
+      inputCep: ""
+    };
+  },
+  methods: {
+    foundNewInputCep() {
+      searchCep("65081264").then(data => {
+        console.log(data);
+      });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.is-fullwidth {
+  width: 100vh;
+}
+</style>
---
+++ src/views/Cep.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container is-fullheight is-fullcenter">
+    <search-cep-form />
+  </div>
+</template>
+
+<script>
+import SearchCepForm from "@/components/SearchCepForm";
+
+export default {
+  components: {
+    SearchCepForm
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.is-fullheight {
+  height: 100vh;
+}
+.is-fullcenter {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
