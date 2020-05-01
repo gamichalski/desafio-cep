fdd82694c7805a2b89cb4db34218b5e85c9b3958
Thiago Ferreira <thiagofer93@gmail.com>
2020-05-01 16:32:37 -0300

    Conclusão desafio

---
+++ src/components/ListaCep.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="is-fullwidth">
+    <h2 class="subtitle is-fullwidth">Histórico de buscas:</h2>
+    <ul v-for="(cep, index) in ceps" :key="index">
+      <li>{{ cep.logradouro }}, {{ cep.bairro }}, {{ cep.cidade }} - {{ cep.uf }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    ceps: Array
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
